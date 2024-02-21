a33b3343b7192283ed1dbd61bf55bda3284aab3e
Halil Barak <hbarak36@gmail.com>
2024-02-21 21:09:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1 className="text-center text-2xl lg:text-5xl mx-10 font-semibold mt-64 flex-wrap">Welcome to <span className=" bg-gradient-to-l to-green-500 from-blue-400 rounded-lg p-1">Halil Barak</span>'s Personal Website</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
